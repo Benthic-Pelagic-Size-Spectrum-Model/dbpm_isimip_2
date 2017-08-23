1021768f11148456ce6c57f583b1cd93429b92b3
Just Berkhout <just.berkhout@utas.edu.au>
2017-08-23 13:32:39 +1000

    introduce tests based on unit testing framework testthat

---
+++ tests/run_tests.R
@@ -0,0 +1,2 @@
+library(testthat)
+test_file("tests/test-dbpm_fishmip.R")
---
+++ tests/test-dbpm_fishmip.R
@@ -0,0 +1,22 @@
+context("dbpm fishmip model run code ")
+
+test_that("model date boundaries are as expected",{
+  source("../model_date_boundaries.R", local = TRUE)
+  
+  expect_that(start_of_spinup, equals(1))
+  expect_that(end_of_spinup, equals(300*12))
+  expect_that(start_of_history, equals(300*12 + 1))
+  expect_that(end_of_history, equals(300*12 + 55*12))
+  expect_that(start_of_projections, equals(300*12 + 55*12 + 1))
+  expect_that(end_of_projections, equals(5412))
+  
+  expect_that(start_of_spinup_weeks, equals(1))
+  expect_that(end_of_spinup_weeks, equals(300*48))
+  expect_that(start_of_history_weeks, equals(300*48 + 1))
+  expect_that(end_of_history_weeks, equals(300*48 + 55*48))
+  expect_that(start_of_projections_weeks, equals(300*48 + 55*48 + 1))
+  expect_that(end_of_projections_weeks, equals(21645))  
+  
+})
+
+
