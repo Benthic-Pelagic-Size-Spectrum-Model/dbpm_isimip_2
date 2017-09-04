6b5f2c1c377ae91084e7622906da6e0cf8d10307
Just Berkhout <just.berkhout@utas.edu.au>
2017-09-04 16:31:07 +1000

    Adjust plot_means for history plot_means (timeseries plots) no sources it's data from _history_ and _rcp_ files

--- plot_means.R
+++ plot_means.R
@@ -7,10 +7,6 @@ library(gridExtra)
 
 area_means <- function(run, varname, nc_path="/rd/gem/private/fishmip_outputs/aug_2017_2/netcdf", area="Global"){
 
-#  nc_path="/rd/gem/private/fishmip_outputs/aug_2017_2/netcdf"
-#  run <- "rcp26"
-#  varname <- "tcb"
-  
   nc_filename <- sprintf("%s/dbpm_ipsl-cm5a-lr_%s_no-fishing_no-oa_%s.nc", nc_path, run, varname)
   
   the_brick <- brick(nc_filename)
@@ -32,22 +28,15 @@ area_means <- function(run, varname, nc_path="/rd/gem/private/fishmip_outputs/au
     the_brick <- mask(the_brick, eez)
   }
   
-#  plot(the_brick[[1]])
-#  plot(masked_brick[[1]])  # gives viz of extent
-#  plot(masked_brick[[6]])  # gives viz of extent
-  
   my_means <- data.frame(cellStats(the_brick, mean))
   names(my_means) <- run
-
+  my_means$timestep <- the_brick@z[[1]]
+  
   return (my_means)
 }
 
 plot_area_means <- function(runs, varname, nc_path="/rd/gem/private/fishmip_outputs/aug_2017_2/netcdf", area="Global", smooth=FALSE){
-  
-#  varname <- "tcb"
-#  runs <- c("rcp26","rcp45","rcp60","rcp85")
-#  nc_path="/rd/gem/private/fishmip_outputs/aug_2017_2/netcdf"
-  
+
   source("helpers.R", local = TRUE)
   numcores <- length(runs)
   the_cluster <- parallel::makeForkCluster(getOption("cl.cores", numcores))
@@ -61,12 +50,14 @@ plot_area_means <- function(runs, varname, nc_path="/rd/gem/private/fishmip_outp
   
   parallel::stopCluster(the_cluster)
   
+  df_wide <- data.frame(timestep=0:1811)
+  for(i in 1:length (output)) {
+    df_wide <- plyr::join(df_wide, output[[i]], by="timestep", type="left")
+  }
   
-  df <- do.call("cbind", output)
-  df$timestep <- seq.int(nrow(df))
   
-  df <- reshape2::melt(df, id="timestep", variable.name = "scenario", value.name = "value")
-
+  df <- reshape2::melt(df_wide, id="timestep", variable.name = "scenario", value.name = "value", na.rm=TRUE)
+  
   months <- seq(as.Date("1950/1/1"),as.Date("2100/12/31"), by = "month")  
   #months[start_of_projections - end_of_spinup]
   
@@ -76,12 +67,12 @@ plot_area_means <- function(runs, varname, nc_path="/rd/gem/private/fishmip_outp
   the_plot <- ggplot(df) 
   the_plot <-  the_plot + ggtitle(the_title)
   if(smooth==TRUE)
-    the_plot <- the_plot + geom_smooth(aes(x=months[timestep], y=value, colour=scenario),  method = 'loess')
+    the_plot <- the_plot + geom_smooth(aes(x=months[timestep+1], y=value, colour=scenario),  method = 'loess')
   else
-    the_plot <- the_plot + geom_line(aes(x=months[timestep], y=value, colour=scenario))
+    the_plot <- the_plot + geom_line(aes(x=months[timestep+1], y=value, colour=scenario))
   
-    
-  the_plot <- the_plot + scale_colour_manual(values=c("red","green","black", "blue")) + 
+  
+  the_plot <- the_plot + scale_colour_manual(values=c("grey", "red","green","black", "blue")) + 
     geom_vline(xintercept = as.numeric(months[start_of_projections - end_of_spinup]), col = "red", linetype="dotted") + 
     xlab('time') +
     ylab(varname)
@@ -89,15 +80,14 @@ plot_area_means <- function(runs, varname, nc_path="/rd/gem/private/fishmip_outp
   return(the_plot)
 }
 
-myvar <- "tcb"
-myruns <- c("rcp26","rcp45","rcp60","rcp85")
-#myruns <- c("rcp85")
+myvar <- "tsb"
+myruns <- c("history", "rcp26", "rcp45", "rcp60", "rcp85")
 
 
-p1 <- plot_area_means(myruns, myvar, smooth = TRUE)
-p2 <- plot_area_means(myruns, myvar)
-p3 <- plot_area_means(myruns, myvar, area="AusEEZ", smooth = TRUE)
-p4 <- plot_area_means(myruns, myvar, area="AusEEZ")
+p1 <- plot_area_means(myruns, myvar, smooth = TRUE, nc_path = "/rd/gem/public/fishmip/netcdf")
+p2 <- plot_area_means(myruns, myvar, nc_path = "/rd/gem/public/fishmip/netcdf")
+p3 <- plot_area_means(myruns, myvar, area="AusEEZ", smooth = TRUE, nc_path = "/rd/gem/public/fishmip/netcdf")
+p4 <- plot_area_means(myruns, myvar, area="AusEEZ", nc_path = "/rd/gem/public/fishmip/netcdf")
 
 gridExtra::grid.arrange(p1, p2, p3, p4, ncol=2)
 
