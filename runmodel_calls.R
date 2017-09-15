7d6068c5152e015891a95c5990df40c547f7861a
Just Berkhout <just.berkhout@utas.edu.au>
2017-09-15 13:18:33 +1000

    add dryRun flag and functionality, additional validation won't run or error if no cells to process

--- runmodel_calls.R
+++ runmodel_calls.R
@@ -1,11 +1,7 @@
-options(warn=-1) 
+#options(warn=-1) 
 
 #if (!require("GetoptLong")) install.packages("GetoptLong", repos ="https://cran.csiro.au/")
-
 rm(list=ls())
-
-source('runmodel.R')
-source('helpers.R')
 
 #set defaults 
 startCell <- 1
@@ -13,7 +9,7 @@ endCell <- 1
 run <- "rcp26"
 maxCores <- 1
 VERSION <- "0.01 (25-Aug-2017)"
-
+dryRun <- FALSE
 
 #handle commandline parameters
 GetoptLong::GetoptLong(
@@ -22,29 +18,44 @@ GetoptLong::GetoptLong(
   "maxCores=i", "Maximum of cores to be used",
   "run=s", "Emissions scenario (rcp26, rcp45, rcp60, rcp85), optional, default (rcp26)",
   "inputPath=s", "Path where well-known input files are available, mandatory",
-  "outputPath=s", "Path where output files can and will be written, mandatory"
+  "outputPath=s", "Path where output files can and will be written, mandatory",
+  "dryRun!", "if set, the long running model code will not be run; all other code will run (default = FALSE, i.e. will run all)"
 )
 
 #validations
 if (startCell < 1 | startCell > 39567 ) stop(sprintf("value out of range for 'startCell' '%s'", startCell))
 if (endCell < 1 | endCell > 39567 ) stop(sprintf("value out of range for 'endCell' '%s'", endCell))
 if (startCell > endCell)  stop(sprintf("'endCell' ('%s') must be greater than 'startCell ('%s')", endCell, startCell))
+if (!exists("inputPath")) stop
+if (!exists("outputPath")) stop
+
+
+# start execution
+source('runmodel.R')
+source('helpers.R')
+
 
 cells <- seq(startCell, endCell, by = 1)
 cells <- cells[!cells %in% grid_ids_found(outputPath)]
 
-numCores <- ifelse(length(cells) > maxCores, maxCores, length(cells))
-
-the_cluster <- parallel::makeForkCluster(getOption("cl.cores", numCores))
-
-discard_output <- parallel::clusterApplyLB(the_cluster
-               ,x=cells
-               ,fun=rungridsep
-               ,gcm="ipsl-cm5a-lr"
-               ,run=run
-               ,output="aggregated"
-               ,input_files_location = inputPath
-               ,output_files_location = outputPath)
-
-parallel::stopCluster(the_cluster)
+if (length(cells) > 0) {
+  
+  numCores <- ifelse(length(cells) > maxCores, maxCores, length(cells))
+  
+  the_cluster <- parallel::makeForkCluster(getOption("cl.cores", numCores))
+  
+  if(!dryRun) {
+    discard_output <- parallel::clusterApplyLB(the_cluster
+                                               ,x=cells
+                                               ,fun=rungridsep
+                                               ,gcm="ipsl-cm5a-lr"
+                                               ,run=run
+                                               ,output="aggregated"
+                                               ,input_files_location = inputPath
+                                               ,output_files_location = outputPath)
+  }
+  
+  parallel::stopCluster(the_cluster)
+  
+}
 
