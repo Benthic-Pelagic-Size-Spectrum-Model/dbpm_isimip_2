54900b1c68b6d069fc80c6c26fcc4b8cc64c0470
Camilla Novaglio <camillan@gem48.novalocal>
2021-03-30 11:29:23 +1100

    keep list of linux command

---
+++ LinuxCommands.R
@@ -0,0 +1,5 @@
+# useful linux command that you keep forgetting: 
+# ls | wc -l # count files in folder 
+# find . -mindepth 1 -print -quit # print name of first file in folder 
+# print size of first file in folder # ls -l -h dbpm_output_all_13047_ssp585.rds
+# find file greater than 2.9M in current directory # find . -maxdepth 1 -type f -size +3M
