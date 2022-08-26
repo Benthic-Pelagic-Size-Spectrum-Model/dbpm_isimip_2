d82916299cbee205ab2bc9d4568bf9a9754819f6
camillanovaglio <camilla.novaglio@gmail.com>
2022-08-26 14:23:24 +1000

    Prepare inputs for LME scale runs

--- getinputsISIMIP3b.R
+++ getinputsISIMIP3b.R
@@ -1,3 +1,4 @@
+
 rm(list=ls())
 #install.packages('RNetCDF', lib = '/home/rhenegha/R_package_library', repos = 'https://cran.rstudio.com/')
 #install.packages('reshape2', lib = '/home/rhenegha/R_package_library', repos = 'https://cran.rstudio.com/')
@@ -12,27 +13,87 @@ library(abind) #library(abind, lib.loc = '/home/rhenegha/R_package_library')
 # CN - CN has not run this step yet. this step for CMIP6 similulations was run by Ryan who then provided the inputs 
 # explore LME inputs provided by Ryan 
 
-lme<-read.csv("/rd/gem/private/fishmip_inputs/ISIMIP3a/lme_inputs/obsclim/0.25deg/gfdl-mom6-cobalt2_obsclim_tos_degC_15arcmin_LME_42_monthly_1961_2010.csv")
-head(lme)
-class(lme)
-colnames(lme)
-lme[1:3, 1:6]
-nrow(lme)
+library(raster)
+library(stringr)
 
-lmeRaster<-lme[,c(2,1,4)]
+file_path<-"/rd/gem/private/fishmip_inputs/ISIMIP3a/lme_inputs/obsclim/0.25deg"
+file_name<-"gfdl-mom6-cobalt2_obsclim_tos_degC_15arcmin_LME_42_monthly_1961_2010.csv"
+lme<-read.csv(file.path(file_path, file_name))
+# class(lme)
+# colnames(lme)
+# lme[1:3, 1:6]
+# nrow(lme)
+
+# this seems ok - raster is created so that dim 1 is long, dim 2 is lat and dim 3 is time...  
+lmeRaster<-lme[,c(2,1,4:ncol(lme))]
 lmeRaster <- rasterFromXYZ(lmeRaster)
-pdf("/data/home/camillan/dbpm/Output/plot.pdf")
-plot(lmeRaster)
-dev.off()
+# dim(lmeRaster)
+# ncol(lme) # with lat, lon and area 
+
+# CHECK 
+# trial<-lme[,c(2,1,6)]
+# trial <- rasterFromXYZ(trial)
+# dim(trial)
+
+# pdf("/data/home/camillan/dbpm/Output/plot.pdf")
+# plot(lmeRaster[[3]])
+# dev.off()
+
+# pdf("/data/home/camillan/dbpm/Output/plot1.pdf")
+# plot(trial)
+# dev.off()
 
 # TO 
 # do the weighted average of climate input (for all inputs including depth)
+# mean values - weighted by grid cell latitude 
+# https://stackoverflow.com/questions/55230510/calculating-weighted-spatial-global-annual-averages-across-grid-cells-using-netc
+
+# raster with latitude cell values 
+w <- init(lmeRaster, 'y')
+# cosine after transforming to radians
+w <- cos(w  * (pi/180))
+# multiply weights with values
+x <- lmeRaster * w
+
+# # need to maks the weights too otherwise denominator too high
+# pdf("Output/plot1.pdf", height = 8, width = 6)
+# plot(x[[1]]) # MULTIPLE DIMENTION
+# dev.off()
+# 
+# pdf("Output/plot2.pdf", height = 8, width = 6)
+# plot(w) # ONE DIMENTION 
+# dev.off()
+
+w2<-mask(w, lmeRaster, updatevalue=NA) # MULTIPLE DIMENTIONS 
+# w2<-mask(w, lmeRaster[[1]], updatevalue=NA) # ONE DIMENTION - it does not seem to matter but need to check 
+
+# pdf("Output/plot3.pdf", height = 8, width = 6)
+# plot(w2) 
+# dev.off()
+
+# compute weighted average - WARNING is na.rm = TRUE ok??  
+weighted_mean<-cellStats(x, sum, na.rm = TRUE) / cellStats(w2, sum, na.rm = TRUE)
+# class(weighted_mean)
+# weighted_mean[1:10]
+
+weighted_mean<-data.frame(Year = colnames(lme[,-c(1,2,3)]), weighted_mean = weighted_mean)
+weighted_mean$LME <- str_extract(file_name,"(?<=LME_).+(?=_monthly)")# extract number after LME_
+rownames(weighted_mean)<-NULL
+
+# Calculate the spin up 
+
+
 # calculate the total area of LME (save this as part of the inputs file - one file per inputs)
-# do the sum of effort by LME and / by total area of LME
+lme[1:2,1:4]
+weighted_mean$area_m2<-sum(lme$area_m2)
+
+# do the sum of effort by LME and / by total area of LME - LOAD FASER READING FUNCION... 
+effort<-read.csv("/rd/gem/private/users/yannickr/DKRZ_EffortFiles/effort_histsoc_1841_2010.csv")
+# do the above for 67 LME so that you can add area in effort here and for all LMEs 
 
 # end result: 1 estimate by month of all inputs - one value only for depth 
 
-# go to step 2 
+# go to step 2 - batch create inputs. 
 
 
 
